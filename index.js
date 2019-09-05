a53d61d78c20b37b98333cd8175cc9d1980be901
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-09-05 15:46:26 -0400

    more player movement added. legal moves adjusted

--- index.js
+++ index.js
@@ -1,90 +1,3 @@
-// menu
-// ***************************************************
-// game title
-// intro to game
-// click 'get started' button -> to team selection
-
-
-
-// eventually...
-// **************************************************
-// team build section 
-// player one selects team Composition
-// clicks ready button
-// player two selects team Composition
-// clicks ready button
-// can select team comp at random - button
-// click 'to battle' button 
-
-
-
-// game logic
-// **************************************************
-// player one selects character
-
-// pick action section
-// three options:
-//     move
-//     attack
-//     pass
-
-// ********************
-// if move:
-// characters possible movement squares highlight - light green
-//     initally character can only move in four cardinal directions
-// if p1 selects a possible movement square the game prompts question 'Are you sure you want to move here?'
-//     if yes, character moves to open space
-//         then ends turn, p2 goes
-//     if no resets back to p1 pick action section
-// if p1 selects occupied space
-//     game alerts 'cant move there'
-//     then resets back to p1 pick action section
-
-// ********************
-// if attack:
-// checks 4 cardinal directions for a p2c
-// if p2c there, square turns red 
-
-// if p1 character clicks p2 chracter, battle prompt initiates instead
-//     i.e 'p1 Warrior will attack p2 warrior. Do you want to continue?'
-//     if yes, p1c attacks p2c - initiate battle logic
-//     if no, resets back to pick action section
-
-
-
-// battle logic
-// **************************************************
-// accept another character as a parameter
-// determine if hit lands or not
-//     use hit percentage mechanism
-//     do a math random to get a num from 0-.99 and compare to characters hit % to see if hit lands
-// damage - roll random number on a dice
-//     dice mechanism makes damage more realistic because two dice allows for more 5-9 and less outliers
-// armor - subtract damage done from armor stat to get actual damage
-// health - subract actual damage from p2c
-// check if p2c has zero health
-//     if so, p2c dead 
-// check if p2 has any characters left alive
-//     if yes, p2 turn
-//     if no, game over p1 wins 
-
-
-// **************************************************
-// if pass:
-// next players turn
-
-
-// ************* ideas **************
-// - during battle have fight simulation going on screen.
-//  instead of clicking fight each time just have an auto timer going
-// - Select Player function? Would need to change all warriors/soldiers to player/opponent 
-//  and include if statement about choosing class
-
-
-
-
-
-
 class Character {
     constructor(health, hit, strength, armor, pieceCode) {
         // this.name = name;
@@ -115,14 +28,10 @@ class Character {
     // }
 }
 
-// '&#x1F6E1'
-// './PNG/Warrior.png'
-// './PNG/Soldier.png'
-// '&#9876'
 
-// Pieces
-let warrior = new Character(100, 40, 9, 2, './PNG/Warrior.png'); //player 1 for now
-let soldier = new Character(150, 60, 3, 7, './PNG/Soldier.png'); // player 2 for now
+// Pieces/Images
+let warrior = new Character(100, 100, 9, 2, './PNG/Warrior.png'); //player 1 for now
+let soldier = new Character(150, 100, 3, 7, './PNG/Soldier.png'); // player 2 for now
 let grass = './PNG/grass-png-cartoon.png'
 // let fighter = new Character(75, 75, 9, 2);
 
@@ -133,30 +42,9 @@ function printToScreen() {
 }
 
 function fight() {
-    let fightButton = document.getElementById('fight-button');
     // let restartButton = document.getElementById('restart-button');
-    let gameMessage = document.getElementById('game-message');
-
     warriorAttack();
-    gameMessage.innerText = 'Soldier will attack now'
-
-    if (isGameOver(soldier.health)) {
-        endGame(`Warrior has killed the Soldier`)
-        return;
-    }
-
-    fightButton.disabled = true;
-
-    setTimeout(() => {
-        soldierAttack();
-        fightButton.disabled = false;
-        gameMessage.innerText = 'Warrior will attack now'
-
-        if (isGameOver(warrior.health)) {
-            endGame(`Soldier has killed the Warrior`)
-            return;
-        }
-    }, 1000)
+    soldierAttack();
 }
 
 function warriorAttack() {
@@ -197,17 +85,11 @@ function soldierAttack() {
 }
 
 // returns a boolean: true is health reaches zero or less
-function isGameOver(health) {
+function isDead(health) {
     return health <= 0;
 };
 
-function endGame(message) {
-    document.getElementById('game-message').innerText = message;
-    document.getElementById('restart-button').hidden = false;
-    document.getElementById('fight-button').hidden = true;
-}
-
-function restart () {
+function restart() {
     document.location.href = '';
 }
 
@@ -228,11 +110,11 @@ let id = 0
 function createBoard() {
     let board = document.getElementById('gameboard');
 
-    for(let r=0; r < 5; r++){
+    for (let r = 0; r < 5; r++) {
         let row = document.createElement('div');
         row.setAttribute('class', 'row');
 
-        for(let c=0; c<5; c++){
+        for (let c = 0; c < 5; c++) {
             let cell = document.createElement('div');
             let theImage = document.createElement('img')
             // theImage.setAttribute('src', ' ')
@@ -242,27 +124,30 @@ function createBoard() {
 
             // cell.id = id;
             row.appendChild(cell);
-            if(r === 0){
+            if (r === 0) {
                 // cell.innerHTML = warrior.pieceCode; //just to lable each cell. later this becomes the game pieces
                 // cell.appendChild(theImage)
                 theImage.setAttribute('class', 'battleImage')
+                theImage.setAttribute('alt', 'player2')
                 theImage.src = warrior.pieceCode;
                 theImage.width = "100";
             }
-            if(r === 4){
+            if (r === 4) {
                 // cell.innerHTML = soldier.pieceCode; //just to lable each cell. later this becomes the game pieces
                 // cell.appendChild(theImage)
                 theImage.setAttribute('class', 'battleImage')
+                theImage.setAttribute('alt', 'player1')
                 theImage.src = soldier.pieceCode;
                 theImage.width = "100";
             }
-            if(r > 0 && r < 4){
+            if (r > 0 && r < 4) {
                 theImage.setAttribute('class', 'grassImage')
+                theImage.setAttribute('alt', '')
                 theImage.src = grass;
                 theImage.width = "100";
             }
         }
-    board.appendChild(row);
+        board.appendChild(row);
     }
 }
 
@@ -270,48 +155,94 @@ createBoard()
 
 
 
-
-
-
 let board = document.getElementsByClassName('gameboard')[0];
 let cells = document.querySelectorAll('img')
-
-// const pieces = {
-//     warPiece: '&#9876',
-//     solPiece: '&#x1F6E1', //placeholder
-//     // fightPiece: 'ffff' //placeholder
-// }
 
 
 let clickedPiece = null;
 
-let turns = 1
+let turn = 1
 
 cells.forEach(function (element) {
     element.addEventListener('click', function (elem) {
-        // console.log(elem, elem.target.src);
-        if (elem.target.className === 'battleImage') { //and does not equal any other pieces &&& this is one of my neighbots
-            clickedPiece = elem.target.src
-            // console.log(clickedPiece)
-            elem.target.src = grass;
-            elem.target.setAttribute('class', 'grassImage');
-            findNeighbors('before')
-            $('.battleImage').removeAttr('id')
-            elem.target.setAttribute('id', '');
-
-        } else if (elem.target.className !== 'battleImage' && clickedPiece != null) {
-            // console.log(clickedPiece)
-            elem.target.src = clickedPiece;
-            elem.target.setAttribute('id', 'moving');
-            elem.target.setAttribute('class', 'battleImage');
-            clickedPiece = ''
-            findNeighbors('after')
-
-            // switch turns 
-            turns++
-        }
+        player1Turn(elem);
+        player2Turn(elem);
+        //movePlayer(elem);
     })
 });
+
+function player1Turn(elem) {
+    if (elem.target.alt === 'player1' && turn % 2 !== 0) {
+        movePlayer(elem);
+    } else if (clickedPiece !== null && turn % 2 !== 0) {
+        movePlayer(elem);
+    }
+}
+
+function player2Turn(elem) {
+    if (elem.target.alt === 'player2' && turn % 2 === 0) {
+        movePlayer(elem);
+    } else if (clickedPiece !== null && turn % 2 === 0) {
+        movePlayer(elem);
+    }
+}
+
+
+let middleOfTurn = false;
+
+function movePlayer(elem) {
+    console.log(elem, turn);
+    middleOfTurn = true;
+
+    if (elem.target.className === 'battleImage') { //and does not equal any other pieces &&& this is one of my neighbots
+        clickedPiece = elem.target.src
+        // console.log(clickedPiece)
+        elem.target.src = grass;
+        elem.target.setAttribute('id', 'moving');
+        elem.target.setAttribute('class', 'grassImage');
+        $('.battleImage').removeAttr('id')
+        $('img').removeAttr('name')
+
+        let neighbors = findNeighbors('before') //weird one
+        neighbors.forEach(neighbor => {
+            console.log(neighbor)
+            if (neighbor.className == 'grassImage') {
+                neighbor.setAttribute('name', 'legal')
+            }
+        })
+        elem.target.setAttribute('id', '');
+        $('.grassImage').removeAttr('alt');
+    } else if (elem.target.className !== 'battleImage' && clickedPiece != null && elem.target.className == 'grassImage' && elem.target.getAttribute('name') == 'legal') {
+        // console.log(clickedPiece)
+        $('img').removeAttr('name')
+        const altAttribute = turn % 2 == 0 ? "player2" : "player1";
+        elem.target.src = clickedPiece;
+        elem.target.setAttribute('id', 'moving');
+        elem.target.setAttribute('class', 'battleImage');
+        elem.target.setAttribute('alt', altAttribute);
+        clickedPiece = null;
+
+        let neighbors = findNeighbors('after')
+        fightNeighbor(neighbors)
+        turn++
+        // $("#turn").html(turn)
+        // middleOfTurn = false;
+        console.log(turn)
+    }
+}
+
+function fightNeighbor(neighbors) {
+    $('.battleImage').removeAttr('data')
+    neighbors.forEach(neighbor => {
+        console.log(neighbor)
+        if (neighbor.className == 'battleImage') {
+            neighbor.setAttribute('data', 'war')
+            fight();
+        }
+    })
+}
+
+
 
 function findNeighbors(when) {
 
@@ -324,28 +255,24 @@ function findNeighbors(when) {
             activeColumn = i;
         }
     })
+    console.log('active column', activeColumn)
 
     let actives = []
-    if((activeColumn + 1) % 5 !== 0){
-         actives.push(columns[activeColumn + 1])
-    } 
+    if ((activeColumn + 1) % 5 !== 0) {
+        actives.push(columns[activeColumn + 1])
+    }
 
-    if(activeColumn % 5 !== 0){
+    if (activeColumn % 5 !== 0) {
         actives.push(columns[activeColumn - 1])
     }
 
-    if(activeColumn < 20) {
+    if (activeColumn < 20) {
         actives.push(columns[activeColumn + 5])
     }
 
-    if(activeColumn > 6){
+    if (activeColumn > 6) {
         actives.push(columns[activeColumn - 5])
     }
     console.log(when, 'neighbors are ', actives)
-
+    return actives
 }
-
-// function movingColor () {
-//     let movingElement = document.getElementById('moving');
-//     movingElement.classList.toggle('moving-color');
-// }
